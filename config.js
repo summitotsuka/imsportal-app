77077da4c2231f45f6daae9801febbe49f98f8fa
CHONDAN <taximeter96@hotmail.com>
2026-09-03 10:20:33 +0700

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const CONFIG = {
   APP_SHORT_NAME: 'IMS',
   VERSION: '1.0.0',
 
-  API_URL: 'https://script.google.com/macros/s/AKfycbzJLxtSznc1ozR-UMLme7apjJyiCIkJnEpdsytZpZWD79NIAqWKJnFPXtdPXzZcZi-DIQ/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbz7pK92-K2fQhN3r7zBGo8XgWkVZR4TdDc88qg8qhwPqDNVOnkgUfTwBDN5Qdj2g5W9oA/exec'
 };
